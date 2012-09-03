bea1b3ada192915499838857da9423f5f4aa219e
jonasac <jonasac@student.matnat.uio.no>
2012-09-03 19:58:06 +0200

    Started work on chargenerator

--- no/uio/ifi/cflat/chargenerator/CharGenerator.java
+++ no/uio/ifi/cflat/chargenerator/CharGenerator.java
@@ -13,45 +13,63 @@ import no.uio.ifi.cflat.log.Log;
  * Module for reading single characters.
  */
 public class CharGenerator {
-    public static char curC, nextC;
-	
-    private static LineNumberReader sourceFile = null;
-    private static String sourceLine;
-    private static int sourcePos;
-	
-    public static void init() {
-	try {
-	    sourceFile = new LineNumberReader(new FileReader(Cflat.sourceName));
-	} catch (FileNotFoundException e) {
-	    Error.error("Cannot read " + Cflat.sourceName + "!");
-	}
-	sourceLine = "";  sourcePos = 0;  curC = nextC = ' ';
-	readNext();  readNext();
-    }
-	
-    public static void finish() {
-	if (sourceFile != null) {
-	    try {
-		sourceFile.close();
-	    } catch (IOException e) {
-		Error.error("Could not close source file!");
-	    }
-	}
-    }
-	
-    public static boolean isMoreToRead() {
-	//-- Must be changed in part 0:
-	return false;
-    }
-	
-    public static int curLineNum() {
-	return (sourceFile == null ? 0 : sourceFile.getLineNumber());
-    }
-	
-    public static void readNext() {
-	curC = nextC;
-	if (! isMoreToRead()) return;
+  public static char curC, nextC;
 
-	//-- Must be changed in part 0:
+  private static LineNumberReader sourceFile = null;
+  private static String sourceLine;
+  private static int sourcePos;
+
+  public static void init() {
+    try {
+      sourceFile = new LineNumberReader(new FileReader(Cflat.sourceName));
+    } catch (FileNotFoundException e) {
+      Error.error("Cannot read " + Cflat.sourceName + "!");
     }
+    sourceLine = "";  sourcePos = 0;  curC = nextC = ' ';
+    readNext();  readNext();
+  }
+
+  public static void finish() {
+    if (sourceFile != null) {
+      try {
+        sourceFile.close();
+      } catch (IOException e) {
+        Error.error("Could not close source file!");
+      }
+    }
+  }
+
+  public static boolean isMoreToRead() {
+    //-- Must be changed in part 0:
+    return (sourceLine != null);
+  }
+
+  public static int curLineNum() {
+    return (sourceFile == null ? 0 : sourceFile.getLineNumber());
+  }
+
+  public static void readNext() {
+    curC = nextC;
+    if (! isMoreToRead()) return;
+    // Must be changed in part 0:
+    if (sourcePos < (sourceLine.length() - 1)) {
+      sourcePos++;
+    } else {
+      try { 
+        sourceLine = sourceFile.readLine();
+      } catch (IOException e) {
+        System.out.println("Something failed");
+        // give reasonable error
+      }
+      while (sourceLine.startsWith("#")) {
+        try {
+          sourceLine = sourceFile.readLine();
+          System.out.println(sourceLine);
+        } catch (IOException e) {
+          System.out.println("Something failed");
+        }
+      }
+    }
+    nextC = sourceLine.charAt(sourcePos);
+  }
 }
