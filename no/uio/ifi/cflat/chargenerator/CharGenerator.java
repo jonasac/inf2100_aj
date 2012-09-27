5c070b9930473f9ddd7c1c73d89e88d64425dae7
Alexander Rødseth <rodseth@gmail.com>
2012-09-27 20:43:52 +0200

    Added eclipse project again. Works here at home. Also minor java changes.

--- no/uio/ifi/cflat/chargenerator/CharGenerator.java
+++ no/uio/ifi/cflat/chargenerator/CharGenerator.java
@@ -1,102 +1,115 @@
+
 package no.uio.ifi.cflat.chargenerator;
 
 /*
  * module CharGenerator
  */
 
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
 import no.uio.ifi.cflat.cflat.Cflat;
 import no.uio.ifi.cflat.error.Error;
 import no.uio.ifi.cflat.log.Log;
+
 
 /*
  * Module for reading single characters.
  */
 public class CharGenerator {
-    public static char curC, nextC;
 
-    private static LineNumberReader sourceFile = null;
-    private static String sourceLine;
-    private static int sourcePos;
-    private static String lastLine;
+	public static char curC, nextC;
 
-    private static final char EOFCHAR = 0x4;
+	private static LineNumberReader sourceFile = null;
+	private static String sourceLine;
+	private static int sourcePos;
 
-    public static void init() {
-        try {
-            sourceFile = new LineNumberReader(new FileReader(Cflat.sourceName));
-        } catch (FileNotFoundException e) {
-            Error.error("Cannot read " + Cflat.sourceName + "!");
-        }
-        sourceLine = "";  sourcePos = 0;  curC = nextC = ' ';
-        readNext();  readNext();
-    }
+	private static final char EOFCHAR = 0x4;
 
-    public static void finish() {
-        if (sourceFile != null) {
-            try {
-                sourceFile.close();
-            } catch (IOException e) {
-                Error.error("Could not close source file!");
-            }
-        }
-    }
+	public static void init() {
+		try {
+			sourceFile = new LineNumberReader(new FileReader(Cflat.sourceName));
+		} catch (FileNotFoundException e) {
+			Error.error("Cannot read " + Cflat.sourceName + "!");
+		}
+		sourceLine = "";
+		sourcePos = 0;
+		curC = nextC = ' ';
+		readNext();
+		readNext();
+	}
 
-    /**
-     * reads one line from sourceFile, will reset sourcePos to null and log the new line
-     */
-    private static void readOneLine() {
-        try { 
-            Log.noteSourceLine(curLineNum(), sourceLine);
-            sourceLine = sourceFile.readLine();
-            sourcePos = 0;
-        } catch (IOException e) {
-            Error.error(e.toString());
-        }
-    }
+	public static void finish() {
+		if (sourceFile != null) {
+			try {
+				sourceFile.close();
+			} catch (IOException e) {
+				Error.error("Could not close source file!");
+			}
+		}
+	}
 
-    /**
-     * Checks if we are beyond the end of the current line
-     * @returns true if we have read the whole sourceLine
-     */
-    private static boolean isEOL() {
-        /*
-	 * Does not look at length - 1 but length because this function checks if one is beyond the length of the line
-         * (which means that we are done reading every single character with readNext)
+	/**
+	 * reads one line from sourceFile, will reset sourcePos to null and log the
+	 * new line
 	 */
-        return (sourcePos == sourceLine.length());
-    }
+	private static void readOneLine() {
+		try {
+			Log.noteSourceLine(curLineNum(), sourceLine);
+			sourceLine = sourceFile.readLine();
+			sourcePos = 0;
+		} catch (IOException e) {
+			Error.error(e.toString());
+		}
+	}
 
-    /** 
-     * Checks if there is more content to read. Handles EOF as well.
-     * @returns true if there is another character available, false if not
-     */
-    public static boolean isMoreToRead() {
-        return (!(sourceLine == null && curC == EOFCHAR));
-    }
+	/**
+	 * Checks if we are beyond the end of the current line
+	 * 
+	 * @returns true if we have read the whole sourceLine
+	 */
+	private static boolean isEOL() {
+		/*
+		 * Does not look at length - 1 but length because this function checks if
+		 * one is beyond the length of the line (which means that we are done
+		 * reading every single character with readNext)
+		 */
+		return (sourcePos == sourceLine.length());
+	}
 
-    public static int curLineNum() {
-        return (sourceFile == null ? 0 : sourceFile.getLineNumber());
-    }
+	/**
+	 * Checks if there is more content to read. Handles EOF as well.
+	 * 
+	 * @returns true if there is another character available, false if not
+	 */
+	public static boolean isMoreToRead() {
+		return (!(sourceLine == null && curC == EOFCHAR));
+	}
 
-    /**
-     * reads the next char from sourceLine, will set the nextC vairable to EOFCHAR if there are no more chars in the file
-     */
-    public static void readNext() {
-        curC = nextC;
-        if (!isMoreToRead()) return;
-        
-        while (sourceLine != null && (sourceLine.startsWith("#") || sourceLine.isEmpty() || isEOL())) {
-            readOneLine();
-        }
+	public static int curLineNum() {
+		return (sourceFile == null ? 0 : sourceFile.getLineNumber());
+	}
 
-        if (sourceLine == null) {
-            nextC = EOFCHAR;
-            return;
-        }
+	/**
+	 * reads the next char from sourceLine, will set the nextC variable to EOFCHAR
+	 * if there are no more chars in the file
+	 */
+	public static void readNext() {
+		curC = nextC;
+		if (!isMoreToRead())
+			return;
 
-        nextC = sourceLine.charAt(sourcePos);
-        sourcePos++;
-    }
+		while (sourceLine != null && (sourceLine.startsWith("#") || sourceLine.isEmpty() || isEOL())) {
+			readOneLine();
+		}
+
+		if (sourceLine == null) {
+			nextC = EOFCHAR;
+			return;
+		}
+
+		nextC = sourceLine.charAt(sourcePos);
+		sourcePos++;
+	}
 }
-
--- no/uio/ifi/cflat/scanner/Token.java
+++ no/uio/ifi/cflat/scanner/Token.java
@@ -1,3 +1,4 @@
+
 package no.uio.ifi.cflat.scanner;
 
 /*
@@ -7,91 +8,91 @@ package no.uio.ifi.cflat.scanner;
 /*
  * The different kinds of tokens read by Scanner.
  */
-public enum Token { 
-    addToken, assignToken, 
-    commaToken, 
-    divideToken, doubleToken,
-    elseToken, eofToken, equalToken, 
-    forToken, 
-    greaterEqualToken, greaterToken, 
-    ifToken, intToken, 
-    leftBracketToken, leftCurlToken, leftParToken, lessEqualToken, lessToken, 
-    multiplyToken, 
-    nameToken, notEqualToken, numberToken, 
-    rightBracketToken, rightCurlToken, rightParToken, returnToken, 
-    semicolonToken, subtractToken, 
-    whileToken;
+public enum Token {
+	addToken, assignToken, commaToken, divideToken, doubleToken, elseToken, eofToken, equalToken, forToken, greaterEqualToken, greaterToken, ifToken, intToken, leftBracketToken, leftCurlToken, leftParToken, lessEqualToken, lessToken, multiplyToken, nameToken, notEqualToken, numberToken, rightBracketToken, rightCurlToken, rightParToken, returnToken, semicolonToken, subtractToken, whileToken;
 
-    // Rel operators
-    private static final Token[] RELOPR = {equalToken, notEqualToken, lessToken, lessEqualToken, greaterToken, greaterEqualToken};
-    private static final Token[] TERMOPR = {addToken, subtractToken};
-    private static final Token[] FACTOROPR = {multiplyToken, divideToken};
-    private static final Token[] OPR = {numberToken};
+	// Rel operators
+	private static final Token[] RELOPR = {
+			equalToken, notEqualToken, lessToken, lessEqualToken, greaterToken, greaterEqualToken };
+	private static final Token[] TERMOPR = {
+			addToken, subtractToken };
+	private static final Token[] FACTOROPR = {
+			multiplyToken, divideToken };
+	private static final Token[] OPR = {
+		numberToken };
 
-    /**
-     * Check if a given token exists in a given list of tokens
-     *
-     * @param tokens A list of tokens
-     * @param t A token
-     * @return True or false
-     */
-    private static boolean has(Token[] tokens, Token t) {
-	for (Token cur : tokens) {
-	    if (t.equals(cur)) {
-		return true;
-	    }
+	/**
+	 * Check if a given token exists in a given list of tokens
+	 * 
+	 * @param tokens
+	 *          A list of tokens
+	 * @param t
+	 *          A token
+	 * @return True or false
+	 */
+	private static boolean has(Token[] tokens, Token t) {
+		for (Token cur : tokens) {
+			if (t.equals(cur)) {
+				return true;
+			}
+		}
+		return false;
 	}
-	return false;
-    }
 
-    /**
-     * Check if a given token exists in a given list of tokens
-     *
-     * @param tokens A list of tokens
-     * @param t A token
-     * @return True or false
-     */
-    public static boolean isFactorOperator(Token t) {
-	return has(FACTOROPR, t);
-    }
+	/**
+	 * Check if a given token exists in a given list of tokens
+	 * 
+	 * @param tokens
+	 *          list of tokens
+	 * @param t
+	 *          token
+	 * @return True or false
+	 */
+	public static boolean isFactorOperator(Token t) {
+		return has(FACTOROPR, t);
+	}
 
-    /**
-     * Checks if a given token is a term opr
-     *
-     * @param t A token
-     * @return True or false
-     */
-    public static boolean isTermOperator(Token t) {
-	return has(TERMOPR, t);
-    }
+	/**
+	 * Checks if a given token is a term opr
+	 * 
+	 * @param t
+	 *          token
+	 * @return True or false
+	 */
+	public static boolean isTermOperator(Token t) {
+		return has(TERMOPR, t);
+	}
 
-    /**
-     * Checks if a given token is a rel opr
-     *
-     * @param t A token
-     * @return True or false
-     */
-    public static boolean isRelOperator(Token t) {
-	return has(RELOPR, t);
-    }
+	/**
+	 * Checks if a given token is a rel opr
+	 * 
+	 * @param t
+	 *          token
+	 * @return True or false
+	 */
+	public static boolean isRelOperator(Token t) {
+		return has(RELOPR, t);
+	}
 
-    /**
-     * Checks if a given token is an opr
-     *
-     * @param t A token
-     * @return True or false
-     */
-    public static boolean isOperand(Token t) {
-	return has(OPR, t);
-    }
+	/**
+	 * Checks if a given token is an opr
+	 * 
+	 * @param t
+	 *          token
+	 * @return True or false
+	 */
+	public static boolean isOperand(Token t) {
+		return has(OPR, t);
+	}
 
-    /**
-     * Checks if a given token is a type name
-     *
-     * @param t A token
-     * @return True or false
-     */
-    public static boolean isTypeName(Token t) {
-	return (t.equals(intToken) || t.equals(doubleToken));
-    }
+	/**
+	 * Checks if a given token is a type name
+	 * 
+	 * @param t
+	 *          A token
+	 * @return True or false
+	 */
+	public static boolean isTypeName(Token t) {
+		return (t.equals(intToken) || t.equals(doubleToken));
+	}
 }
