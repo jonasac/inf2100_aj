cb44429a525bc712528aced2fede84539dafc2a2
Alexander Rødseth <rodseth@gmail.com>
2012-11-28 22:19:50 +0100

    Minor changes to the Python script, for funsies

--- errortest.py
+++ errortest.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python2
+
 import glob
 import subprocess
-our_compiler = "Cflat.jar"
-testfiles = glob.glob('errortests/*/*.cflat')
 
-for file in testfiles:
-  comparison_file = file.replace(".cflat", ".message")
-  print file + "#########################################"
-  subprocess.call(["java", "-jar", our_compiler, file])
-  subprocess.call(['cat', comparison_file])
-  print "################################################\n"
+def main():
+  our_compiler = "Cflat.jar"
+  for filename in glob.glob('errortests/*/*.cflat'):
+    comparison_file = filename.replace(".cflat", ".message")
+    print("### " + filename + " " + "#" * 40)
+    subprocess.call(["java", "-jar", our_compiler, filename])
+    subprocess.call(['cat', comparison_file])
+    print("#" * 45 + "\n")
+
+if __name__ == "__main__":
+  main()
